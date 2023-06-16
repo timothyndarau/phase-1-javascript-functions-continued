f391dc03467e8f0b8c80994e3b7569435586c14e
timothy <timothy.ndarau@student.moringaschool.com>
2023-06-16 10:28:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    if (activity === 'roller-skate') {
+      return `This Saturday, I want to roller-skate!`    
+    }else {
+       return `This Saturday, I want to bathe my dog!`
+    }
+ }
+ function mondayWork(activity='go to the office'){
+    if(activity ==='go to the office'){
+        return 'This Monday, I will go to the office.'
+    }
+    else{
+        return 'This Monday, I will work from home.'
+    }
+ }
+ let wrapAdjective = function() {
+            return function(adjective) {
+              if (adjective === "a hard worker") {
+                return "You are *a hard worker*!";
+              } else if (adjective === "a dedicated programmer") {
+                return "You are ||a dedicated programmer||!";
+              }
+            };
+          };
+
+   
+
+
